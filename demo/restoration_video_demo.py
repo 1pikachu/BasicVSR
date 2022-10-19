70682fa37e6a6961aab4457035ed4b0161b7deb0
Du, Jun <jun.du@intel.com>
2022-10-19 08:33:25 +0000

    enable BasicVSR jit/nv_fuser/channels_last/device/precision

--- demo/restoration_video_demo.py
+++ demo/restoration_video_demo.py
@@ -41,11 +41,8 @@ def parse_args():
         type=int,
         default=None,
         help='maximum sequence length if recurrent framework is used')
-    # parser.add_argument('--device', type=int, default=0, help='CUDA device id')
-    # only 1
-    #parser.add_argument('--batch_size', default=1, type=int, help='batch size')
-    # must float32
-    #parser.add_argument('--precision', default="float32", type=str, help='precision')
+    parser.add_argument('--batch_size', default=1, type=int, help='batch size')
+    parser.add_argument('--precision', default="float32", type=str, help='precision')
     parser.add_argument('--channels_last', default=1, type=int, help='Use NHWC or not')
     parser.add_argument('--jit', action='store_true', default=False, help='enable JIT')
     parser.add_argument('--profile', action='store_true', default=False, help='collect timeline')
@@ -54,8 +51,26 @@ def parse_args():
     parser.add_argument('--device', default='cpu', type=str, help='cpu, cuda or xpu')
     parser.add_argument('--nv_fuser', action='store_true', default=False, help='enable nv fuser')
     args = parser.parse_args()
+    # only 1
+    args.batch_size = 1
+    # must float32
+    args.precision = "float32"
     print(args)
     return args
+
+def trace_handler(p):
+    output = p.key_averages().table(sort_by="self_cpu_time_total")
+    print(output)
+    import pathlib
+    timeline_dir = str(pathlib.Path.cwd()) + '/timeline/'
+    if not os.path.exists(timeline_dir):
+        try:
+            os.makedirs(timeline_dir)
+        except:
+            pass
+    timeline_file = timeline_dir + 'timeline-' + str(torch.backends.quantized.engine) + \
+            '-' + str(p.step_num) + '-' + str(os.getpid()) + '.json'
+    p.export_chrome_trace(timeline_file)
 
 def inference(args):
     if args.nv_fuser:
@@ -67,16 +82,103 @@ def inference(args):
     total_time = 0.0
     total_sample = 0
 
-    for i in range(args.num_iter + args.num_warmup):
-        elapsed = time.time()
-        output = restoration_video_inference(args, args.model, args.input_dir,
-                                             args.window_size, args.start_idx,
-                                             args.filename_tmpl, args.max_seq_len, i)
-        elapsed = time.time() - elapsed
-        print("Iteration: {}, inference time: {} sec.".format(i, elapsed), flush=True)
-        if i >= args.num_warmup:
-            total_sample += args.batch_size
-            total_time += elapsed
+    if args.profile and args.device == "xpu":
+        for i in range(args.num_iter + args.num_warmup):
+            elapsed = time.time()
+            with torch.autograd.profiler_legacy.profile(enabled=args.profile, use_xpu=True, record_shapes=False) as prof:
+                output = restoration_video_inference(args, args.model, args.input_dir,
+                                                     args.window_size, args.start_idx,
+                                                     args.filename_tmpl, args.max_seq_len, i)
+            torch.xpu.synchronize()
+            elapsed = time.time() - elapsed
+            print("Iteration: {}, inference time: {} sec.".format(i, elapsed), flush=True)
+            if i >= args.num_warmup:
+                total_sample += args.batch_size
+                total_time += elapsed
+            if args.profile and i == int((args.num_iter + args.num_warmup)/2):
+                import pathlib
+                timeline_dir = str(pathlib.Path.cwd()) + '/timeline/'
+                if not os.path.exists(timeline_dir):
+                    try:
+                        os.makedirs(timeline_dir)
+                    except:
+                        pass
+                torch.save(prof.key_averages().table(sort_by="self_xpu_time_total"),
+                    timeline_dir+'profile.pt')
+                torch.save(prof.key_averages(group_by_input_shape=True).table(),
+                    timeline_dir+'profile_detail.pt')
+    elif args.profile and args.device == "cuda":
+        with torch.profiler.profile(
+            activities=[torch.profiler.ProfilerActivity.CPU, torch.profiler.ProfilerActivity.CUDA],
+            record_shapes=True,
+            schedule=torch.profiler.schedule(
+                wait=int((args.num_iter + args.num_warmup)/2),
+                warmup=2,
+                active=1,
+            ),
+            on_trace_ready=trace_handler,
+        ) as p:
+            for i in range(args.num_iter + args.num_warmup):
+                elapsed = time.time()
+                with torch.jit.fuser(fuser_mode):
+                    output = restoration_video_inference(args, args.model, args.input_dir,
+                                                         args.window_size, args.start_idx,
+                                                         args.filename_tmpl, args.max_seq_len, i)
+                torch.cuda.synchronize()
+                elapsed = time.time() - elapsed
+                p.step()
+                print("Iteration: {}, inference time: {} sec.".format(i, elapsed), flush=True)
+                if i >= args.num_warmup:
+                    total_sample += args.batch_size
+                    total_time += elapsed
+    elif args.profile and args.device == "cpu":
+        with torch.profiler.profile(
+            activities=[torch.profiler.ProfilerActivity.CPU],
+            record_shapes=True,
+            schedule=torch.profiler.schedule(
+                wait=int((args.num_iter + args.num_warmup)/2),
+                warmup=2,
+                active=1,
+            ),
+            on_trace_ready=trace_handler,
+        ) as p:
+            for i in range(args.num_iter + args.num_warmup):
+                elapsed = time.time()
+                output = restoration_video_inference(args, args.model, args.input_dir,
+                                                     args.window_size, args.start_idx,
+                                                     args.filename_tmpl, args.max_seq_len, i)
+                elapsed = time.time() - elapsed
+                p.step()
+                print("Iteration: {}, inference time: {} sec.".format(i, elapsed), flush=True)
+                if i >= args.num_warmup:
+                    total_sample += args.batch_size
+                    total_time += elapsed
+    elif not args.profile and args.device == "cuda":
+        for i in range(args.num_iter + args.num_warmup):
+            elapsed = time.time()
+            with torch.jit.fuser(fuser_mode):
+                output = restoration_video_inference(args, args.model, args.input_dir,
+                                                     args.window_size, args.start_idx,
+                                                     args.filename_tmpl, args.max_seq_len, i)
+            torch.cuda.synchronize()
+            elapsed = time.time() - elapsed
+            print("Iteration: {}, inference time: {} sec.".format(i, elapsed), flush=True)
+            if i >= args.num_warmup:
+                total_sample += args.batch_size
+                total_time += elapsed
+    else:
+        for i in range(args.num_iter + args.num_warmup):
+            elapsed = time.time()
+            output = restoration_video_inference(args, args.model, args.input_dir,
+                                                 args.window_size, args.start_idx,
+                                                 args.filename_tmpl, args.max_seq_len, i)
+            if args.device == "xpu":
+                torch.xpu.synchronize()
+            elapsed = time.time() - elapsed
+            print("Iteration: {}, inference time: {} sec.".format(i, elapsed), flush=True)
+            if i >= args.num_warmup:
+                total_sample += args.batch_size
+                total_time += elapsed
 
     latency = total_time / total_sample * 1000
     throughput = total_sample / total_time
--- run.sh
+++ run.sh
@@ -3,4 +3,4 @@ mim install mmcv-full==1.6.0
 python setup.py develop
 
 # cpu too slow
-python demo/restoration_video_demo.py  --device cuda --precision float32 --num_iter 2 --num_warmup 1
+python demo/restoration_video_demo.py  --device cuda --precision float32 --num_iter 3 --num_warmup 0 --profile
