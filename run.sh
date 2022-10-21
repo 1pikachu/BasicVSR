00088d6ccf4a7e8c89b62599af7cd3ecbb7c808e
y <baochen.yang@intel.com>
2022-10-21 09:07:17 +0000

    update xpu

--- run.sh
+++ run.sh
@@ -1,6 +1,10 @@
 pip install openmim
-mim install mmcv-full==1.6.0
+mim install mmcv-full==1.6.0	# cuda
+mim install mmcv-full==1.3.13   # atsm
 python setup.py develop
 
 # cpu too slow
+# must float32
 python demo/restoration_video_demo.py  --device cuda --precision float32 --num_iter 3 --num_warmup 0 --profile
+
+python demo/restoration_video_demo.py  --device xpu --precision float32 --num_iter 3 --num_warmup 0
